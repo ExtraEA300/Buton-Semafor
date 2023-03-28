8e9b0eeef123759adf2f53554c636a7ea020d6e0
Walter Bottesch <124290515+ExtraEA300@users.noreply.github.com>
2023-03-28 06:02:34 +0100

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,15 @@
+Buton semafor
+Să se creeze o pagină care conține un buton. La încărcarea paginii butonul va avea culoarea roșie.
+
+După prima apăsare culoarea lui se va schimba în galben.
+
+După a doua apăsare culoarea lui va deveni verde.
+
+După a treia va deveni din nou roșu
+
+Și așa mai departe.
+
+Pentru a schimba culoarea butonului se recomandă folosirea bootstrap.
+
+Variantă bonus
+Culoarea butonului nu se schimbă doar la click, ci se schimbă și din 10 în 10 secunde.
---
+++ semafor.js
@@ -0,0 +1,20 @@
+function loadTrafficLight () {
+	var trafficLight = document.getElementById("semafor");
+	var timer = setInterval(changeColor, 10000);
+	trafficLight.onclick = changeColor;
+}
+
+function changeColor () {
+	var semafor1 = document.getElementById("semafor")
+	var color = semafor1.getAttribute("class");
+	if(color == "red") {
+		semafor1.setAttribute("class", "yellow");
+	} else if(color == "yellow") {
+		semafor1.setAttribute("class", "green");	
+	} else {
+		semafor1.setAttribute("class", "red");		
+	}
+
+}
+
+window.onload = loadTrafficLight;
